fe0a204438ed0b506ce1c27d70b1d9f377910b54
yujinsak <sakeugene@gmail.com>
2025-04-30 14:39:31 +0800

    Add skeleton files for springboot project

---
+++ src/main/java/com/boost/voucher_api/VoucherApiApplication.java
@@ -0,0 +1,13 @@
+package com.boost.voucher_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherApiApplication.class, args);
+	}
+
+}
